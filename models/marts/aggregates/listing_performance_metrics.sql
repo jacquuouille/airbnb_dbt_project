dabf8a41a86c2f316fd3e4c764f48378785758b5
jacquuouille <jacqueshervochon@gmail.com>
2026-05-20 16:57:32 +0200

    Adding num_reviews to marts with tests

--- models/marts/aggregates/listing_performance_metrics.sql
+++ models/marts/aggregates/listing_performance_metrics.sql
@@ -1,30 +1,37 @@
-{{config(
+{{ config(
     materialized='table',
     schema='marts'
 ) }}
 
-with base as (
+with calendar as (
+    select * from {{ ref('fct_calendar') }}
+),
 
-    select 
-        c.listing_id, 
-        l.host_id,
+listings as (
+    select * from {{ ref('dim_listings') }}
+),
+
+reviews as (
+    select * from {{ ref('dim_reviews') }}
+),
+
+base as (
+    select
+        c.listing_id,
         round(
-            100.0*(sum(case when c.is_available = 'true' then 1 else 0 end) / count(c.listing_id)), 2
+            100.0 * (sum(case when c.is_available = 'false' then 1 else 0 end) / count(c.listing_id)), 2
         ) as occupancy_rate_pct,
-        avg(l.review_scores_rating) as avg_rating,
-        avg(l.review_scores_accuracy) as avg_accuracy,
-        avg(l.review_scores_cleanliness) as avg_cleanliness,
-        avg(l.review_scores_checkin) as avg_checkin,
-        avg(l.review_scores_communication) as avg_communication,
-        avg(l.review_scores_location) as avg_location,
-        avg(l.review_scores_value) as avg_value
-    from 
-        {{ ref('fct_calendar') }} c
-    join 
-        {{ ref('dim_listings') }} l on c.listing_id = l.listing_id 
-    group by 
-        1, 2
-
+        count(distinct r.review_id) as num_reviews,
+        max(l.review_scores_rating)            as avg_rating,
+        max(l.review_scores_accuracy)          as avg_accuracy,
+        max(l.review_scores_cleanliness)       as avg_cleanliness,
+        max(l.review_scores_checkin)           as avg_checkin,
+        max(l.review_scores_communication)     as avg_communication,
+        max(l.review_scores_location)          as avg_location
+    from calendar c
+    join listings l on c.listing_id = l.listing_id
+    join reviews r on c.listing_id = r.listing_id
+    group by 1
 )
 
 select * from base
